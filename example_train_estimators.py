248f0a620998236149998d5c5cd1a7d24dc8cc83
Viktor Kazakov <viktorkazakov@outlook.com>
2018-03-06 23:59:35 +0000

    update test example

--- example_train_estimators.py
+++ example_train_estimators.py
@@ -1,5 +1,5 @@
 from mleap.data import Data
-from mleap.data.estimators import instantiate_default_estimators
+from mleap.estimators.estimators import instantiate_default_estimators
 from mleap.experiments import Orchestrator
 
 
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     input_io = data.open_hdf5('data/delgado.hdf5', mode='r')
     out_io = data.open_hdf5('data/classification.hdf5', mode='a')
     dts_names_list, dts_names_list_full_path = data.list_datasets(hdf5_io=input_io, hdf5_group='delgado_datasets/')
-    split_dts_list = data.split_datasets(hdf5_in=input_io, hdf5_out=out_io, dataset_paths=dts_names_list_full_path)
-    instantiated_models = instantiate_default_estimators(estimators=['Classification'], verbose=1)
+    split_dts_list = data.split_datasets(hdf5_in=input_io, hdf5_out=out_io, dataset_paths=dts_names_list_full_path[0:1])
+    instantiated_models = instantiate_default_estimators(estimators=['BaggingClassifier'], verbose=1)
     orchest = Orchestrator(hdf5_input_io=input_io, 
                            hdf5_output_io=out_io, 
                            dts_names=dts_names_list,
