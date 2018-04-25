193d231ba1bc237dbc90ab67fa148ba808afa753
Viktor Kazakov <viktorkazakov@outlook.com>
2018-04-25 21:50:35 +0100

    working code for downloading datasets from openml

--- download_openml.py
+++ download_openml.py
@@ -8,30 +8,34 @@ import sys
 apikey = 'd2b1d13981d4abfb22895337baca924c'
 openml.config.apikey = apikey
 openml.config.set_cache_directory(os.path.expanduser('~/.openml/cache'))
+NUMBER_OF_INSTANCES_CUTOFF_NUMBER = 10000 #
 
-classification_tasks = openml.tasks.list_tasks(task_type_id=1)
-regression_tasks = openml.tasks.list_tasks(task_type_id=2)
+all_datasets = openml.datasets.list_datasets()
 
 data = Data()
 input_io = data.open_hdf5('data/openml.h5', mode='a')
 
-for id in classification_tasks.keys():
-    print(f"Trying to download dataset {id}, {classification_tasks[id]['name']}")
-
-     #ignore too big datasets
-    if float(classification_tasks[id]['NumberOfInstances']) > 10000:
-        print(f"skipping dataset {id}, {classification_tasks[id]['name']}. It is too big")
+for id in all_datasets.keys():
+    #regression datasets have a value of -1. Classification datasets specify the number of classes
+    if all_datasets[id]['NumberOfClasses'] == -1:
+        print(f"Skipping dataset {id}, {all_datasets[id]['name']}. This is a regression dataset.")
         continue
+    if all_datasets[id]['NumberOfMissingValues'] > 0:
+        print(f"Skipping dataset {id}, {all_datasets[id]['name']} due to missing values.")
+        continue
+
+    if all_datasets[id]['NumberOfInstances'] > NUMBER_OF_INSTANCES_CUTOFF_NUMBER:
+        print(f"Skipping dataset {id}, {all_datasets[id]['name']}. It has more than {NUMBER_OF_INSTANCES_CUTOFF_NUMBER} instances.")
+        continue
+
+    print(f"Trying to download dataset {id}, {all_datasets[id]['name']}")
 
     try:
         dataset = openml.datasets.get_dataset(id)
         X, names = dataset.get_data(return_attribute_names=True)
 
         #ignore datasets with empty values 
-        num_missing_values = float(dataset.__dict__['qualities']['NumberOfMissingValues'])
-        if num_missing_values > 0:
-            print(f"skipping dataset {id}, {dataset.__dict__['name']} Missing values")
-            continue
+        
 
         metadata = {
             'class_name': dataset.__dict__['default_target_attribute'],
@@ -42,13 +46,13 @@ for id in classification_tasks.keys():
         class_name_index = names.index(metadata['class_name'])
 
         #Normalize the data
-        scaler = preprocessing.StandardScaler(copy=True, with_mean=True, with_std=True)
-        scaler.fit(X)
-        x_transformed  = scaler.transform(X)
-        x_transformed[:,class_name_index] = X[:, class_name_index]
+        # scaler = preprocessing.StandardScaler(copy=True, with_mean=True, with_std=True)
+        # scaler.fit(X)
+        # x_transformed  = scaler.transform(X)
+        # x_transformed[:,class_name_index] = X[:, class_name_index]
 
         #Convert to DataFrame
-        result = pd.DataFrame(x_transformed)
+        result = pd.DataFrame(X)
         result.columns=names
         result[metadata['class_name']] =  result[metadata['class_name']].astype(int)
 
@@ -58,4 +62,4 @@ for id in classification_tasks.keys():
     except KeyboardInterrupt:
         sys.exit()
     except:
-        print(f'Cannot save dataset {id}')
+        print(f"Cannot save dataset {id}, {all_datasets[id]['name']}")
