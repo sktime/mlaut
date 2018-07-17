1a59f823a3f926f52cfdcb847abf276eca22aa86
Viktor Kazakov <viktorkazakov@outlook.com>
2018-07-17 10:55:08 +0100

    latest changes

--- example_train_estimators.py
+++ example_train_estimators.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     out_io = data.open_hdf5('data/openml-classification.h5', mode='a')
     dts_names_list, dts_names_list_full_path = data.list_datasets(hdf5_io=input_io, hdf5_group='openml/')
     split_dts_list = data.split_datasets(hdf5_in=input_io, hdf5_out=out_io, dataset_paths=dts_names_list_full_path)
-    instantiated_models = instantiate_default_estimators(estimators=['K_Neighbours'], verbose=1, n_jobs=-1)
+    instantiated_models = instantiate_default_estimators(estimators=['Classification'], verbose=1, n_jobs=-1)
     orchest = Orchestrator(hdf5_input_io=input_io, 
                            hdf5_output_io=out_io, 
-                           dts_names=dts_names_list,
+                           dts_names=dts_names_list[0:3],
                            original_datasets_group_h5_path='openml/')
     orchest.run(modelling_strategies=instantiated_models, override_saved_models=True)
 
