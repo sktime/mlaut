c4ddc1766e46035d1c413327b1b9cdaa8f361694
Viktor Kazakov <viktorkazakov@outlook.com>
2018-10-11 20:18:46 +0100

    memory experiment

--- deep_nn.py
+++ deep_nn.py
@@ -26,116 +26,116 @@ hyperparameters = {'epochs': [50,100],
                    
 
 
+data = Data()
+input_io = data.open_hdf5('data/delgado.hdf5', mode='a')
+out_io = data.open_hdf5('data/delgado-classification-copy.h5', mode='a')
+dts_names_list, dts_names_list_full_path = data.list_datasets(hdf5_io=input_io, hdf5_group='delgado_datasets/')
+split_dts_list = data.split_datasets(hdf5_in=input_io, hdf5_out=out_io, dataset_paths=dts_names_list_full_path)
 
+
+def keras_model1(num_classes, input_dim):
+    model = OverwrittenSequentialClassifier()
+    model.add(Dense(288, input_dim=input_dim, activation='relu'))
+    model.add(Dense(144, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(12, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model_optimizer = optimizers.Adam(lr=0.001)
+    model.compile(loss='mean_squared_error', optimizer=model_optimizer, metrics=['accuracy'])
+
+    return model
+
+deep_nn_4_layer_thin_dropout = Deep_NN_Classifier(keras_model=keras_model1, 
+                            properties={'name':'NN-4-layer_thin_dropout'})
+
+
+def keras_model2(num_classes, input_dim):
+    nn_deep_model = OverwrittenSequentialClassifier()
+    nn_deep_model.add(Dense(2500, input_dim=input_dim, activation='relu'))
+    nn_deep_model.add(Dense(2000, activation='relu'))
+    nn_deep_model.add(Dense(1500, activation='relu'))
+    nn_deep_model.add(Dense(num_classes, activation='softmax'))
+
+    model_optimizer = optimizers.Adam(lr=0.001)
+    nn_deep_model.compile(loss='mean_squared_error', optimizer=model_optimizer, metrics=['accuracy'])
+    return nn_deep_model
+
+deep_nn_4_layer_wide_no_dropout = Deep_NN_Classifier(hyperparameters=hyperparameters,
+                            keras_model=keras_model2,
+                            properties={'name':'NN-4-layer_wide_no_dropout'})
+
+
+def keras_model3(num_classes, input_dim):
+    nn_deep_model = OverwrittenSequentialClassifier()
+    nn_deep_model.add(Dense(2500, input_dim=input_dim, activation='relu'))
+    nn_deep_model.add(Dense(2000, activation='relu'))
+    nn_deep_model.add(Dropout(0.5))
+    nn_deep_model.add(Dense(1500, activation='relu'))
+    nn_deep_model.add(Dense(num_classes, activation='softmax'))
+
+    model_optimizer = optimizers.Adam(lr=0.001)
+    nn_deep_model.compile(loss='mean_squared_error', optimizer=model_optimizer, metrics=['accuracy'])
+    return nn_deep_model
+
+deep_nn_4_layer_wide_with_dropout = Deep_NN_Classifier(hyperparameters=hyperparameters,
+                            keras_model=keras_model3,
+                            properties={'name':'NN-4-layer_wide_with_dropout'})
+
+
+def keras_model4(num_classes, input_dim):
+    nn_deep_model = OverwrittenSequentialClassifier()
+    nn_deep_model.add(Dense(5000, input_dim=input_dim, activation='relu'))
+    nn_deep_model.add(Dense(4500, activation='relu'))
+    nn_deep_model.add(Dense(4000, activation='relu'))
+    nn_deep_model.add(Dropout(0.5))
+
+    nn_deep_model.add(Dense(3500, activation='relu'))
+    nn_deep_model.add(Dense(3000, activation='relu'))
+    nn_deep_model.add(Dense(2500, activation='relu'))
+    nn_deep_model.add(Dropout(0.5))
+    
+    
+    nn_deep_model.add(Dense(2000, activation='relu'))
+    nn_deep_model.add(Dense(1500, activation='relu'))
+    nn_deep_model.add(Dense(1000, activation='relu'))
+    nn_deep_model.add(Dropout(0.5))
+    
+    nn_deep_model.add(Dense(500, activation='relu'))
+    nn_deep_model.add(Dense(250, activation='relu'))
+    nn_deep_model.add(Dense(num_classes, activation='softmax'))
+    
+    model_optimizer = optimizers.Adam(lr=0.001)
+    nn_deep_model.compile(loss='mean_squared_error', optimizer=model_optimizer, metrics=['accuracy'])
+    return nn_deep_model
+
+deep_nn_12_layer_wide_with_dropout = Deep_NN_Classifier(hyperparameters=hyperparameters,
+                            keras_model=keras_model4,
+                            properties={'name':'NN-12-layer_wide_with_dropout'})
+
+
+estimators = [deep_nn_4_layer_thin_dropout,
+            deep_nn_4_layer_wide_no_dropout, 
+            deep_nn_4_layer_wide_with_dropout,
+            deep_nn_12_layer_wide_with_dropout]
+
+
+
+# estim = instantiate_default_estimators(['SVC'])
+
+# # estimators = []
+# for e in estim:
+#     if e.properties['name'] is not 'NeuralNetworkDeepClassifier':
+#         estimators.append(e)
+
+orchest = Orchestrator(hdf5_input_io=input_io, hdf5_output_io=out_io, dts_names=dts_names_list,
+                original_datasets_group_h5_path='delgado_datasets/')
 
 if __name__ == '__main__':
     multiprocessing.set_start_method('forkserver', force=True)
 
 
-    data = Data()
-    input_io = data.open_hdf5('data/delgado.hdf5', mode='a')
-    out_io = data.open_hdf5('data/delgado-classification-copy.h5', mode='a')
-    dts_names_list, dts_names_list_full_path = data.list_datasets(hdf5_io=input_io, hdf5_group='delgado_datasets/')
-    split_dts_list = data.split_datasets(hdf5_in=input_io, hdf5_out=out_io, dataset_paths=dts_names_list_full_path)
 
-    
-    def keras_model1(num_classes, input_dim):
-        model = OverwrittenSequentialClassifier()
-        model.add(Dense(288, input_dim=input_dim, activation='relu'))
-        model.add(Dense(144, activation='relu'))
-        model.add(Dropout(0.5))
-        model.add(Dense(12, activation='relu'))
-        model.add(Dense(num_classes, activation='softmax'))
-
-        model_optimizer = optimizers.Adam(lr=0.001)
-        model.compile(loss='mean_squared_error', optimizer=model_optimizer, metrics=['accuracy'])
-
-        return model
-
-    deep_nn_4_layer_thin_dropout = Deep_NN_Classifier(keras_model=keras_model1, 
-                                properties={'name':'NN-4-layer_thin_dropout'})
-    
-
-    def keras_model2(num_classes, input_dim):
-        nn_deep_model = OverwrittenSequentialClassifier()
-        nn_deep_model.add(Dense(2500, input_dim=input_dim, activation='relu'))
-        nn_deep_model.add(Dense(2000, activation='relu'))
-        nn_deep_model.add(Dense(1500, activation='relu'))
-        nn_deep_model.add(Dense(num_classes, activation='softmax'))
-
-        model_optimizer = optimizers.Adam(lr=0.001)
-        nn_deep_model.compile(loss='mean_squared_error', optimizer=model_optimizer, metrics=['accuracy'])
-        return nn_deep_model
-
-    deep_nn_4_layer_wide_no_dropout = Deep_NN_Classifier(hyperparameters=hyperparameters,
-                                keras_model=keras_model2,
-                                properties={'name':'NN-4-layer_wide_no_dropout'})
-
-
-    def keras_model3(num_classes, input_dim):
-        nn_deep_model = OverwrittenSequentialClassifier()
-        nn_deep_model.add(Dense(2500, input_dim=input_dim, activation='relu'))
-        nn_deep_model.add(Dense(2000, activation='relu'))
-        nn_deep_model.add(Dropout(0.5))
-        nn_deep_model.add(Dense(1500, activation='relu'))
-        nn_deep_model.add(Dense(num_classes, activation='softmax'))
-
-        model_optimizer = optimizers.Adam(lr=0.001)
-        nn_deep_model.compile(loss='mean_squared_error', optimizer=model_optimizer, metrics=['accuracy'])
-        return nn_deep_model
-
-    deep_nn_4_layer_wide_with_dropout = Deep_NN_Classifier(hyperparameters=hyperparameters,
-                                keras_model=keras_model3,
-                                properties={'name':'NN-4-layer_wide_with_dropout'})
-
-
-    def keras_model4(num_classes, input_dim):
-        nn_deep_model = OverwrittenSequentialClassifier()
-        nn_deep_model.add(Dense(5000, input_dim=input_dim, activation='relu'))
-        nn_deep_model.add(Dense(4500, activation='relu'))
-        nn_deep_model.add(Dense(4000, activation='relu'))
-        nn_deep_model.add(Dropout(0.5))
-
-        nn_deep_model.add(Dense(3500, activation='relu'))
-        nn_deep_model.add(Dense(3000, activation='relu'))
-        nn_deep_model.add(Dense(2500, activation='relu'))
-        nn_deep_model.add(Dropout(0.5))
-        
-        
-        nn_deep_model.add(Dense(2000, activation='relu'))
-        nn_deep_model.add(Dense(1500, activation='relu'))
-        nn_deep_model.add(Dense(1000, activation='relu'))
-        nn_deep_model.add(Dropout(0.5))
-        
-        nn_deep_model.add(Dense(500, activation='relu'))
-        nn_deep_model.add(Dense(250, activation='relu'))
-        nn_deep_model.add(Dense(num_classes, activation='softmax'))
-        
-        model_optimizer = optimizers.Adam(lr=0.001)
-        nn_deep_model.compile(loss='mean_squared_error', optimizer=model_optimizer, metrics=['accuracy'])
-        return nn_deep_model
-
-    deep_nn_12_layer_wide_with_dropout = Deep_NN_Classifier(hyperparameters=hyperparameters,
-                                keras_model=keras_model4,
-                                properties={'name':'NN-12-layer_wide_with_dropout'})
-
-
-    estimators = [deep_nn_4_layer_thin_dropout,
-                deep_nn_4_layer_wide_no_dropout, 
-                deep_nn_4_layer_wide_with_dropout,
-                deep_nn_12_layer_wide_with_dropout]
-
-
-
-    # estim = instantiate_default_estimators(['SVC'])
-
-    # # estimators = []
-    # for e in estim:
-    #     if e.properties['name'] is not 'NeuralNetworkDeepClassifier':
-    #         estimators.append(e)
-
-    orchest = Orchestrator(hdf5_input_io=input_io, hdf5_output_io=out_io, dts_names=dts_names_list,
-                    original_datasets_group_h5_path='delgado_datasets/')
     orchest.run(modelling_strategies=estimators, verbose=True)
 
 
