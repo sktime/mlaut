5a4025996db56dd0d40e0b52c4233684d06cd7e5
Viktor Kazakov <viktorkazakov@outlook.com>
2018-11-06 21:17:27 +0000

    new train script

--- train_in_docker.sh
+++ train_in_docker.sh
@@ -1,1 +1,1 @@
-sudo docker run -ti -d --name mlaut-train -v "$(pwd):/mlaut" mlaut:latest /bin/sh -c 'python deep_nn.py'
+sudo docker run -ti -d --name mlaut-train -v "$(pwd):/mlaut" mlaut:latest /bin/sh -c 'python classic_methods.py'
