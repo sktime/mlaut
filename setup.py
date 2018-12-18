f435e597760a1bb75b9a59cd217275e87f22251e
Viktor Kazakov <viktorkazakov@outlook.com>
2018-12-18 21:25:59 +0000

    cleaning up repository

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='mlaut',
-      version='0.1.2',
+      version='0.1.3',
       description='Automation of large-scale training, evaluation and benchmarking of machine learning algorithms.',
       url='https://viktorkaz.github.io/',
       author='Viktor Kazakov',
@@ -16,14 +16,14 @@ setup(name='mlaut',
                 'mlaut.experiments',
                 'mlaut.shared'],
       install_requires=[
-          'numpy>=1.7',
+          'numpy>=1.15.4',
           'h5py>=2.8'
           'pandas>=0.21',
           'scikit-learn>=0.20.1',
           'scikit-posthocs>=0.3.4', 
           'scipy>=1.0.0', 
           'tables>=3.4.2', 
-          'tensorflow>=1.5.0',
+          'tensorflow>=1.12.0',
           'Orange3>=3.18.0',
           'matplotlib>=2.1.0'
       ],
--- train_in_docker.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run -ti -d --name mlaut-train -v "$(pwd):/mlaut" mlaut:latest /bin/sh -c 'python classic_methods.py'
